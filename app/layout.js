67136e3a1e1dcaccd65e5046784726267c450618
ilyats <iluha3211@gmail.com>
2026-01-14 05:15:55 +0200

    init

---
+++ app/layout.js
@@ -0,0 +1,21 @@
+import Header from '@/components/header/header';
+import './globals.css';
+import HeaderBackground from '@/components/header-background/header-background';
+
+export const metadata = {
+  title: 'NextLevel Food',
+  description: 'Delicious meals, shared by a food-loving community.',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <HeaderBackground />
+        <Header />
+
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ components/header/header.js
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import logoImg from '@/assets/logo.png';
+import Image from "next/image";
+import styles from './header.module.scss';
+import NavLink from "../nav-link/nav-link";
+
+export default function Header() {
+    return (
+        <header className={styles.header}>
+            <Link href="/" className={styles.logo}>
+                <Image src={logoImg} alt="A plate with food on it" preload="preload" priority />
+                NextLevel Food
+            </Link>
+
+            <nav className={styles.nav}>
+                <ul>
+                    <li>
+                        <NavLink href="/meals">Browse Meals</NavLink>
+                    </li>
+                    <li>
+                        <NavLink href="/community">Foodies Community</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
